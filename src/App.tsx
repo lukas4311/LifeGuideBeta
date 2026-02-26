65831fb109b3606e54979fefd23d0211992742fa
Lukas Salficky <lukas4311@gmail.com>
2026-02-26 18:27:07 +0100

    Add route for modul detail

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import ModuleDetailLegacy from "./components/ModuleDetailLegacy";
+import { LanguageProvider, useLanguage } from './components/LanguageContext';
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <ThemeProvider defaultTheme="light">
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
+      <LanguageProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route key={"ModuleDetailLegacy"} path="/ModuleDetailLegacy" element={<ModuleDetailLegacy />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </LanguageProvider>
     </QueryClientProvider>
   </ThemeProvider>
 );
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,14 +2,11 @@
 import React from 'react';
 import AppLayout from '@/components/AppLayout';
 import { AppProvider } from '@/contexts/AppContext';
-import { LanguageProvider, useLanguage } from '../components/LanguageContext';
 
 const Index: React.FC = () => {
   return (
     <AppProvider>
-      <LanguageProvider>
         <AppLayout />
-      </LanguageProvider>
     </AppProvider>
   );
 };
