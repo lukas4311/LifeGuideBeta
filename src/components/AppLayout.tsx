be2a18c3bf5bc392e87760c6a98c67fc1a082d8d
Lukas Salficky <lukas4311@gmail.com>
2026-03-01 16:41:07 +0100

    Hide progress toggle button

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -324,7 +324,7 @@ const AppLayout: React.FC = () => {
       <Footer />
 
       {/* Progress Sidebar */}
-      {showProgressSidebar && (
+      {/* {showProgressSidebar && (
         <>
           <div 
             className="fixed inset-0 bg-black/30 z-40"
@@ -355,17 +355,17 @@ const AppLayout: React.FC = () => {
             </div>
           </div>
         </>
-      )}
+      )} */}
 
       {/* Progress Toggle */}
-      <button
+      {/* <button
         onClick={() => setShowProgressSidebar(!showProgressSidebar)}
         className="fixed bottom-6 right-6 z-40 w-14 h-14 bg-purple-600 text-white rounded-full shadow-lg hover:shadow-xl hover:bg-purple-700 transition-all flex items-center justify-center"
       >
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
         </svg>
-      </button>
+      </button> */}
     </div>
   );
 };
